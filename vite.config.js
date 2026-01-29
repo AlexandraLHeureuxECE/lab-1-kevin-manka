f3dd24ea63456744a739cfa67d638ca67da06788
Kevin Manka <113623001+kevin-manka@users.noreply.github.com>
2026-01-29 13:07:05 -0500

    feat: specify base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/lab-1-kevin-manka/",
   plugins: [
     react(),
     tailwindcss(),
